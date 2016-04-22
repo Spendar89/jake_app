ba50ae991ec6f6cdd61db5ae8ad908833de53f2b
Jake Sendar <jakesendar@gmail.com>
2016-04-22 04:44:56 -0400

    add webpack config and client base files

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    const dude = 'bro';
+
+    return (
+      <h1>Hello, { dude }.</h1>
+    );
+  }
+};
